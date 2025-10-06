701b32d952e4e03c5af892f155631325aefbac22
HaRi-Haran-D <hvj58128@gmail.com>
2025-10-06 20:46:45 +0530

    font changed and form js added

---
+++ register.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+
+const myform = document.getElementById('myform')
+
+const floatingInput = document.getElementById('floatingInput').value;
+const floatingfname = document.getElementById('floatingfname').value;
+//const floatinglname = document.getElementById('floatinglname').value;
+const floatingPassword = document.getElementById('floatingPassword').value;
+
+
+myform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const obj = {username: floatingfname,email: floatingInput, password: floatingPassword}
+
+    const changedata = JSON.stringify(obj);
+    console.log(changedata)
+
+    localStorage.setItem('userdata',changedata);
+    alert("Successfully Registered");
+    window.location.href = "index.html";
+    myform.reset();
+});
+});
