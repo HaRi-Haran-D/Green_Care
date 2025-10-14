a02b8a630cacbdcc96f213fd32bf755c14a3b716
HaRi-Haran-D <hvj58128@gmail.com>
2025-10-14 11:27:05 +0530

    login added

--- register.js
+++ register.js
@@ -9,14 +9,12 @@ myform.addEventListener('submit', (e) => {
     e.preventDefault();
     const floatingInput = document.getElementById('floatingInput').value;
     const floatingfname = document.getElementById('floatingfname').value;
-    //const floatinglname = document.getElementById('floatinglname').value;
     const floatingPassword = document.getElementById('floatingPassword').value;
     
     const obj = {username: floatingfname,email: floatingInput, password: floatingPassword}
 
     const changedata = JSON.stringify(obj);
-    console.log(changedata)
-
+    
     localStorage.setItem('userdata',changedata);
     alert("Successfully Registered");
     window.location.href = "index.html";
